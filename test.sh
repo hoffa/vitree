255e132fc9f9154a153733f45002aaeadbb3f05f
Christoffer Rehn <1280602+hoffa@users.noreply.github.com>
2026-06-03 12:14:17 +0900

    Make test output verbose

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-go test -coverprofile=cover.out .
+go test -v -coverprofile=cover.out .
 pct=$(go tool cover -func=cover.out | awk '/^total:/ {print $3}' | tr -d %)
 echo "coverage: ${pct}%"
 awk -v p="$pct" 'BEGIN{exit (p+0<95)}' || {
